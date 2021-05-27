e78af3a4e5a84d7e0b457761bad38d6a6d29e4d4
Matías <matiiias.-15@live.cl>
2021-05-27 00:18:13 -0400

    Avance vista vitrina
    Se cargan los productos desde la base de datos y se agrega paginación

--- src/app/content/view-store/my-store-view/my-store-view.component.ts
+++ src/app/content/view-store/my-store-view/my-store-view.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationStart, Router, RouterEvent } from '@angular/router';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { filter } from 'rxjs/operators';
+import { ProductService } from 'src/app/_api/product/product.service';
+import { UserService } from 'src/app/_api/user/user.service';
+import { ProductInterface } from 'src/app/_models/product';
 
 @Component({
   selector: 'app-my-store-view',
@@ -10,6 +14,7 @@ import { filter } from 'rxjs/operators';
 })
 export class MyStoreViewComponent implements OnInit {
 
+  @BlockUI('allInfo') blockUIAllInfo: NgBlockUI;
   public product: any;
   public uid: any;
   public carouselTwo = [
@@ -17,9 +22,18 @@ export class MyStoreViewComponent implements OnInit {
     '../../../assets/images/carousel/03.jpg',
     '../../../assets/images/carousel/01.jpg'
   ];
+  public PRODUCT: ProductInterface[];
+  public collectionSize: any;
+  public page = 1;
+  public pageSize = 4;
+  public nameCake;
+  public addressCake;
+
   constructor(
     private router: ActivatedRoute,
-    private config: NgbCarouselConfig
+    private config: NgbCarouselConfig,
+    private productService: ProductService,
+    private userService: UserService
   ) {
     config.interval = 3000;
     config.keyboard = false;
@@ -28,6 +42,27 @@ export class MyStoreViewComponent implements OnInit {
   ngOnInit(): void {
     this.uid = this.router.snapshot.paramMap.get('uid');
     console.log("this.router.snapshot.paramMap", this.router.snapshot.paramMap);
+    this.getFullInfoProduct();
+    this.getInfoUser();
+  }
+
+  getFullInfoProduct() {
+    this.blockUIAllInfo.start('Cargando..');
+    this.productService.getFullInfoProduct(this.uid).subscribe(product => {
+      this.PRODUCT = product;
+      this.collectionSize = this.PRODUCT.length;
+      this.blockUIAllInfo.stop();
+    });
+  }
+
+  getInfoUser() {
+    this.userService.getOneUser(this.uid).subscribe(
+      data => {
+        console.log(data);
+        this.nameCake = data.cakeName;
+        this.addressCake = data.address;
+      }
+    );
   }
 
 }
--- src/app/content/view-store/view-store.module.ts
+++ src/app/content/view-store/view-store.module.ts
@@ -4,6 +4,10 @@ import { MyStoreViewComponent } from './my-store-view/my-store-view.component';
 import { RouterModule } from '@angular/router';
 import { CardModule } from '../partials/general/card/card.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FilterPipeModule } from 'src/app/_pipe/filter-pipe.module';
+import { FormsModule } from '@angular/forms';
+import { BlockUIModule } from 'ng-block-ui';
+import { BlockTemplateComponent } from 'src/app/_layout/blockui/block-template.component';
 
 
 
@@ -13,6 +17,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     CommonModule,
     CardModule,
     NgbModule,
+    FormsModule,
+    BlockUIModule.forRoot({
+      template: BlockTemplateComponent
+    }),
+    FilterPipeModule,
     RouterModule.forChild([
       {
         path: 'mi-tienda/:uid',
